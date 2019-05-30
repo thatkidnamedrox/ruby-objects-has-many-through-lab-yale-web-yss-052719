144f3c5cc84dad76be5526bcf0b841a0f668856b
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-05-30 14:41:32 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,10 +1,18 @@
 class Artist
 
   @@all = []
-  attr_accessor :name
+  attr_accessor :name, :songs
 
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
   def self.all
     @@all
   end
 
+  def new_song(name, genre)
+    song = Song.new(name, genre)
+    
+  end
 end
