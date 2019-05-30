816ab9a6aa5ced1f4f9080fe70c7d1d8776cb0ce
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-05-30 15:10:47 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -2,6 +2,13 @@ class Appointment
   @@all = []
   attr_accessor
 
+  def initialize(date, patient, doctor)
+    @date = date
+    @patient = patient
+    @doctor = doctor
+    @@all << self
+  end
+
   def self.all
     @@all
   end
