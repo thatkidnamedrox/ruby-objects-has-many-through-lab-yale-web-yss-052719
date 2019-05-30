4171ac763d71d217791cf40321f6b116461cb383
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-05-30 14:48:50 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,19 @@
 class Song
 
+  @@all = []
   attr_accessor :name, :artist, :genre
 
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
+
 
 end
