8f4c3bb04e2888c50b5f9565a9555ec8201586a4
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-05-30 15:00:08 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -14,11 +14,13 @@ class Artist
   end
 
   def new_song(name, genre)
-    puts name, genre.inspect
-
+    puts name
+    puts genre.inspect
     song = Song.new(name, self, genre)
     genre.songs << song
     genre.artists << self
+
+    puts genre.inspect
 
     @songs << song
     @genres << genre
