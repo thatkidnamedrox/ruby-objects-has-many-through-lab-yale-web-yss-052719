2966d00038d2b87e3c6a9b5d32adb34cd121a903
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-05-30 14:50:12 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,9 +1,15 @@
 class Genre
 
-  attr_accessor :name
+  @@all = []
+  attr_accessor :name, :songs
 
   def initialize(name)
     @name = name
+    @songs = []
+  end
+
+  def self.all
+    @@all
   end
 
 end
