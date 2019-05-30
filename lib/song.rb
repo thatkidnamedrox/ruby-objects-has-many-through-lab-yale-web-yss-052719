0906a0acff7fe5c2cc5387667021ee4b699988ee
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-05-30 15:03:35 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,7 +5,11 @@ class Song
 
   def initialize(name, artist, genre)
     @name = name
+    
     @artist = artist
+    @artist.songs << self
+    @artist.genres << genre
+    
     @genre = genre
     @@all << self
   end
